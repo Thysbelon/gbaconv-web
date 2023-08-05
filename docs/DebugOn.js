322c06f5024eb4b0852a4355a58c13ba6f66ed92
Thysbelon <dnferch@gmail.com>
2023-08-04 21:03:45 -0400

    Merge normal and debug mode

--- docs/DebugOn.js
+++
@@ -1,1 +0,0 @@
-const DebugOn=true;
--- docs/gbaconv-web-frontend.js
+++ docs/gbaconv-web-frontend.js
@@ -17,7 +17,6 @@ function myClick(e){
 			for (let i=0; i<10; i++) {
 				console.log(data[i])
 			}
-			//FS.mount(IDBFS, {}, '/data');
 			FS.writeFile("/"+filename, data, {flags:"w+"})
 			Module.ccall(
 				"GBAConv",
@@ -26,41 +25,11 @@ function myClick(e){
 				["/"+filename]
 			)
 			if (filename.includes(".srm")){filename=filename.replace(".srm", ".sav")} else {filename=filename.replace(".sav", ".srm")}
-			//const request = indexedDB.open("/data");
-			//request.onsuccess = (event) => {
-			//	db = event.target.result;
-			//	db.transaction("FILE_DATA").objectStore("FILE_DATA").get( "/"+filename ).onsuccess = (event) => {
-			//		var savedataObj=event.target.result.contents
-			//		savedataValues=Object.values(savedataObj) // array
-			//		//const buffer=new ArrayBuffer(romdataValues.length)
-			//		//const view = new Uint8Array(buffer);
-			//		const savedata8array = Uint8Array.from(savedataValues);
-			//		
-			//		const file = new File([savedata8array], e.target.files[0].name.replace(/\.\w\w\w/, "."+filename.slice(-3) ) )
-			//		download(file)
-			//		//var req = indexedDB.deleteDatabase("/data");
-			//		//req.onsuccess = function () {
-			//		//	console.log("Deleted database successfully");
-			//		//};
-			//		//FS.unlink("/data/savedata.sav")
-			//		//FS.unlink("/data/savedata.srm")
-			//		//FS.unmount('/data');
-			//		//FS.rmdir("/data")
-			//		if (typeof DebugOn === 'undefined') {
-			//			window.location.reload(); // I tried so hard to make this app work without reloading, but all of my attempts to delete the contents of memfs failed.
-			//		}
-			//	};
-			//};
 			console.log("filename: "+filename)
-			//stream = FS.open(filename, "r")
-			//FS.close(stream)
-			savedata8array=FS.readFile('/'+filename) // is opts optional?
+			savedata8array=FS.readFile('/'+filename)
 			const file = new File([savedata8array], e.target.files[0].name.replace(/\.\w*$/, "."+filename.slice(-3) ) )
 			download(file)
 			FS.unlink('/'+filename);
-			//if (typeof DebugOn === 'undefined') {
-			//	window.location.reload(); // I tried so hard to make this app work without reloading, but all of my attempts to delete the contents of memfs failed.
-			//}
 			
 		}
 	}
